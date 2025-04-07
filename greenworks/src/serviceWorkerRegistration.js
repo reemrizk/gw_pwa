fa05104e8c9f328cfae585b021f298f22bcb239e
reemrizk <reemrizk244@gmail.com>
2025-04-07 11:04:20 -0400

    service worker

--- greenworks/src/serviceWorkerRegistration.js
+++ greenworks/src/serviceWorkerRegistration.js
@@ -31,6 +31,8 @@ export function register(config) {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      console.log('Service Worker URL:', swUrl);
+
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
